02d7dbeecf69a24845e528aa09403853896e5773
Tankit927 <tonkyponky@proton.me>
2025-03-27 13:29:42 +0530

    Added Ch-12 Exception Handling

---
+++ Ch12/Ch12Q7/Ch12Q7/SquareRoot.cs
@@ -0,0 +1,80 @@
+﻿// Write a program that takes a positive integer from the console and prints 
+// the square root of this integer. If the input is negative or invalid print 
+// "Invalid Number" in the console. In all cases print "Good Bye".
+
+class SquareRoot
+{
+    static void Main()
+    {
+        int num = 0;
+
+        Console.WriteLine("Program to print square root of given integer " +
+        "and print \"Invalid Number\" if integer is invalid or negative " +
+        "and print \"Good Bye in all cases\"");
+
+        while(true)
+        {
+            try
+            {
+                num = GetInt("Num = ");
+                Console.WriteLine();
+                PrintSquareRootOf(num);
+                break;
+            }
+            catch (Exception e) when (e is ArgumentNullException || e is FormatException || e is OverflowException)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Error: {e.Message}");
+                // Console.WriteLine(e.InnerException);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Error: {e.Message}");
+                // Console.WriteLine(e.InnerException);
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("Good Bye.");
+            }
+        }
+    }
+
+
+    static int GetInt(string prompt)
+    {
+        // Method to user input integer 
+        // Throw "Invalid Number" exception if integer is invalid or negative
+
+        int num;
+        
+        while(true)
+        {
+            Console.Write(prompt);
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+                if(num < 0)
+                {
+                    Console.WriteLine($"\nEnter a valid positive integer in range[0,{int.MaxValue}]");
+                    continue;
+                }
+
+                return num;
+            }
+            catch(Exception e)
+            {
+                throw new Exception($"{e.Message}\nEnter a valid positive integer in range[0,{int.MaxValue}]");
+            }
+        }
+    }
+
+
+    static void PrintSquareRootOf(int num)
+    {
+        // Method to print square root of given integer
+
+        Console.WriteLine($"Square root: {Math.Sqrt(num):f2}");
+    }
+}
---
+++ Ch12/Ch12Q8/Ch12Q8/ReadNumberInCertainRange.cs
@@ -0,0 +1,68 @@
+﻿// Write a method ReadNumber(int start, int end) that reads an integer 
+// from the console in the range [start…end]. In case the input integer is 
+// not valid or it is not in the required range throw appropriate exception. 
+// Using this method, write a program that takes 10 integers a1, a2, …, a10 
+// such that 1 < a1 < … < a10 < 100.
+
+class ReadNumberInCertainRange
+{
+    static void Main()
+    {
+        int len = 10;
+        int start = 1; // inclusive
+        int end = 100; // inclusive
+        int[] myArray = new int[len];
+
+        Console.WriteLine("Program to enter 10 integers");
+        for(int i = 0; i < len; i++)
+        {
+            myArray[i] = ReadNumber($"myArray[{i}]: ",i > 0 ? myArray[i-1]+1 : start, end - (len-i) + 1);
+        }
+
+        Console.WriteLine();
+        PrintArray(myArray);
+    }
+
+
+    static void PrintArray(params int[] myArray)
+    {
+        // Method to print given array
+
+        foreach(int i in myArray)
+        {
+            Console.Write($"{i} ");
+        }
+
+        Console.WriteLine();
+    }
+
+
+    static int ReadNumber(string prompt, int start=1, int end=100)
+    {
+        // Method to read integer in range[start, end]
+
+        int num;
+
+        while(true)
+        {
+            Console.Write(prompt);
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+                if(num < start || num > end)
+                {
+                    Console.WriteLine($"Enter integer in range[{start},{end}]");
+                    continue;
+                }
+
+                return num;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Error: {e.Message}");
+                Console.WriteLine($"Enter integer in range[{start},{end}]");
+            }
+        }
+    }
+}
